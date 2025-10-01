b6ed694429eb724b7fde34149ae855088c80acc4
francesca-attindehou <64556004+Cesca19@users.noreply.github.com>
2025-10-01 09:16:37 +0200

    ADD conan installation script for windows; ADD sfml install through conan

--- build_scripts/build_windows.bat
+++ build_scripts/build_windows.bat
@@ -1,66 +1,69 @@
 @echo off
-setlocal
 
-echo === Checking Python installation ===
+setlocal
+set "green=[32m"
+set "blue=[36m"
+set "yellow=[33m"
+set "red=[31m"
+set "white=[0m"
+
+echo %blue%[INFO] Checking Python installation%white%
 python --version >nul 2>nul
 if %errorlevel% neq 0 (
-    echo Python not found. Please install it from https://www.python.org/downloads/
-    echo Then close your command line, re-open it and relaunch the script.
+    echo %red%[ERROR] Python not found. Please install it from https://www.python.org/downloads/%white%
+    echo %red%[ERROR] Then close your command line, re-open it and relaunch the script.%white%
     exit /b 1
 ) else (
-    echo Python already installed.
+    echo %green%[OK] Python already installed.%white%
 )
 
-echo === Checking pip installation    ===
+echo %blue%[INFO] Checking pip installation%white%
 python -m pip --version >nul 2>nul
 if %errorlevel% neq 0 (
-    echo pip not found. Installing...
+    echo %yellow%[WARN] pip not found. Installing...%white%
     python -m ensurepip --upgrade
     python -m pip install --upgrade pip
 ) else (
-    echo pip already installed.
+    echo %green%[OK] pip already installed.%white%
 )
 
-echo === Checking CMake installation  ===
+echo %blue%[INFO] Checking CMake installation%white%
 where cmake >nul 2>nul
 if %errorlevel% neq 0 (
-    echo CMake not found. Please install it from https://cmake.org/download/
-    echo Then close your command line, re-open it and relaunch the script.
+    echo %red%[ERROR] CMake not found. Please install it from https://cmake.org/download/%white%
+    echo %red%[ERROR] Then close your command line, re-open it and relaunch the script.%white%
     exit /b 1
 ) else (
-    echo CMake already installed.
+    echo %green%[OK] CMake already installed.%white%
 )
 
-echo === Checking Conan installation ===
-where conan >nul 2>nul
-if %errorlevel% neq 0 (
-    echo Installing conan.
-    pip install conan
-    set "PATH=%USERPROFILE%\AppData\Roaming\Python\Python39\Scripts;%PATH%"
-    conan install .. --output-folder=. --build=missing
+echo %blue%[INFO] Checking Conan installation%white%
+call "./build_scripts/conan_installer.bat"
 
-    source ~/.profile
-) else (
-    echo Conan already installed.
-)
-
-echo === Configuring Conan profile ===
+echo %blue%[INFO] Configuring Conan profile%white%
 conan profile detect --force
 
-echo === Creating build directory ===
-if exist build rmdir /s /q build
-mkdir build
+echo %blue%[INFO] Installing dependencies with Conan%white%
+conan install . 
+
+echo %blue%[INFO] Configuring project with CMake%white%
 cd build
+cmake .. -DCMAKE_TOOLCHAIN_FILE=generators/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Debug
 
-echo === Installing dependencies with Conan ===
-conan install .. --output-folder=. --build=missing
+echo %blue%[INFO]Building project%white%
+cmake --build . --config Debug
 
-echo === Configuring project with CMake ===
-cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
+:: echo %green%[OK] Build completed successfully (Windows)%white%
 
-echo === Building project ===
-cmake --build . --config Release
 
-echo  Build completed successfully (Windows)
-
+:: if exist .conan_intall_dir rmdir /s /q build
+:: md .conan_intall_dir
+:: python -m pip install conan --target .\.conan_intall_dir\ --upgrade
+:: set conan_exe_dir=%cd% + .conan_intall_dir\
+:: echo conan exe dir is %conan_exe_dir=%cd%%
+:: 
+:: 
+:: 
+:: 
+:: 
 endlocal
