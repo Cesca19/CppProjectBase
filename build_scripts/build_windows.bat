397a78f585dce3605f594ac2ff87874ca5872add
francesca-attindehou <64556004+Cesca19@users.noreply.github.com>
2025-10-03 12:59:46 +0200

    Add -h option ; build mode option(debug or release)

--- build_scripts/build_windows.bat
+++ build_scripts/build_windows.bat
@@ -6,6 +6,16 @@ set "blue=[36m"
 set "yellow=[33m"
 set "red=[31m"
 set "white=[0m"
+set BUILD_TYPE=Debug
+
+:: Handle -h or --help
+if "%~1"=="-h" goto :help
+if "%~1"=="--help" goto :help
+
+if "%~1"=="Debug" set BUILD_TYPE=Debug 
+if "%~1"=="Release" set BUILD_TYPE=Release
+
+echo %blue%[INFO] Build type selected: (%BUILD_TYPE%)%white%
 
 echo %blue%[INFO] Checking Python installation%white%
 python --version >nul 2>nul
@@ -43,27 +53,41 @@ call "./build_scripts/conan_installer.bat"
 echo %blue%[INFO] Configuring Conan profile%white%
 conan profile detect --force
 
-echo %blue%[INFO] Installing dependencies with Conan%white%
-conan install . 
+echo %blue%[INFO] Installing dependencies with Conan (%BUILD_TYPE%)%white%
+conan install .  -s build_type=%BUILD_TYPE% --build=missing
 if %errorlevel% neq 0 (
     echo %red%[ERROR] Conan install failed.%white%
     exit /b 1
 )
 
-echo %blue%[INFO] Configuring project with CMake%white%
+echo %blue%[INFO] Configuring project with CMake (%BUILD_TYPE%)%white%
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release  -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=bin
+cmake .. -DCMAKE_BUILD_TYPE=%BUILD_TYPE%  -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=bin
 if %errorlevel% neq 0 (
     echo %red%[ERROR] CMake configuration failed.%white%
     exit /b 1
 )
 
-echo %blue%[INFO] Building project%white%
-cmake --build . --config Release
+echo %blue%[INFO] Building project (%BUILD_TYPE%)%white%
+cmake --build . --config %BUILD_TYPE%
 if %errorlevel% neq 0 (
     echo %red%[ERROR] Build failed .%white%
     exit /b 1
 )
-echo %green%[OK] Build completed successfully (Windows)%white%
+echo %green%[OK] Build completed successfully (Windows, %BUILD_TYPE%)%white%
+goto :eof
+
+:help
+echo Usage: build_windows.bat [BUILD_TYPE] / --help
+echo.
+echo   BUILD_TYPE can be:
+echo     Debug     - Build with debug symbols
+echo     Release   - Build optimized release
+echo.
+echo Examples:
+echo   build_windows.bat Debug
+echo   build_windows.bat Release
+echo   build_windows.bat --help
+exit /b 0
 
 endlocal
